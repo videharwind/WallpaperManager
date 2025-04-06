70f1e28b65261901bfc5f3d8d81647ad81950f7f
videharwind <videharwind@gmail.com>
2025-04-06 10:56:31 +0530

    Add project files.

---
+++ WallpaperManager/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WallpaperManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            buttonSelect = new Button();
+            ButtonRemove = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.Location = new Point(1076, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 73);
+            button1.TabIndex = 0;
+            button1.Text = "ADD";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Location = new Point(12, 127);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1158, 610);
+            flowLayoutPanel1.TabIndex = 1;
+            flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
+            // 
+            // buttonSelect
+            // 
+            buttonSelect.Location = new Point(27, 12);
+            buttonSelect.Name = "buttonSelect";
+            buttonSelect.Size = new Size(94, 68);
+            buttonSelect.TabIndex = 2;
+            buttonSelect.Text = "Set Wallpaper";
+            buttonSelect.UseVisualStyleBackColor = true;
+            buttonSelect.Click += button2_Click;
+            // 
+            // ButtonRemove
+            // 
+            ButtonRemove.Location = new Point(147, 14);
+            ButtonRemove.Name = "ButtonRemove";
+            ButtonRemove.Size = new Size(94, 68);
+            ButtonRemove.TabIndex = 3;
+            ButtonRemove.Text = "Remove";
+            ButtonRemove.UseVisualStyleBackColor = true;
+            ButtonRemove.Click += ButtonRemove_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1182, 768);
+            Controls.Add(ButtonRemove);
+            Controls.Add(buttonSelect);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button buttonSelect;
+        private Button ButtonRemove;
+    }
+}
---
+++ WallpaperManager/Form1.cs
@@ -0,0 +1,161 @@
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace WallpaperManager
+{
+    public partial class Form1 : Form
+    {
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPI_UPDATEINFILE = 1;
+        public const int SPI_SENDCHANGE = 2;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SystemParametersInfo(uint uAction, uint uParam, String lpvParam, int fuWinIni);
+
+        private PictureBox SelectedPicture = null;
+        public static void SetWallpaper(string imagePath)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPI_UPDATEINFILE | SPI_SENDCHANGE);
+        }
+        private static void SetWallpaper(PictureBox picturebox)
+        {
+
+            string temppath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "TempWallpaper.bmp");
+            picturebox.Image.Save(temppath, System.Drawing.Imaging.ImageFormat.Bmp);
+            SetWallpaper(temppath);
+        }
+        private Image LoadWithoutLock(string path)
+        {
+            using(FileStream stream = new FileStream(path,FileMode.Open,FileAccess.Read,FileShare.ReadWrite))
+            {
+                return Image.FromStream(stream);
+            }
+        }
+        public static string[] getImages(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            string[] images = new string[20];
+            int index = 0;
+            foreach (string i in files)
+            {
+                string extension = Path.GetExtension(i);
+                if (extension == ".jpg" || extension == ".png")
+                {
+                    images[index] = i;
+                    index++;
+                }
+            }
+            string[] im = new string[index];
+            Array.Copy(images, im, index);
+            return im;
+        }
+        public static string CreateAppDataPath()
+        {
+            string commanPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string appPath = Path.Combine(commanPath, "WallpaperManager\\images");
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            return appPath;
+        }
+        public Form1()
+        {
+            CreateAppDataPath();
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string appPath = CreateAppDataPath();
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "ImageFiles (*.png)|*.png";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string source in ofd.FileNames)
+                {
+                    string destinationPath = Path.Combine(appPath, Path.GetFileName(source));
+                    File.Copy(source, destinationPath);
+
+                }
+            }
+            LoadWallpapers();
+        }
+        
+        public void LoadWallpapers()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            string[] im = getImages(CreateAppDataPath());
+            foreach (string images in im)
+            {
+                PictureBox pictures = new PictureBox
+                {
+                    Image = LoadWithoutLock(images),
+                    Width = 320,
+                    Height = 180,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Tag = images,
+                    Margin = new Padding(20),
+                    BorderStyle = BorderStyle.FixedSingle
+
+                };
+
+                pictures.Click += Pictures_Click;
+                flowLayoutPanel1.Controls.Add(pictures);
+            }
+        }
+
+        private void Pictures_Click(object? sender, EventArgs e)
+        {
+            PictureBox Picture = sender as PictureBox;
+            SelectedPicture = Picture;
+            if (SelectedPicture != null)
+            {
+                buttonSelect.BackColor = Color.Green;
+                ButtonRemove.BackColor = Color.Green;
+            }
+            else
+            {
+
+                ButtonRemove.BackColor = Color.White;
+                buttonSelect.BackColor = Color.White;
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadWallpapers();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (SelectedPicture != null)
+            {
+                SetWallpaper(SelectedPicture);
+                SelectedPicture = null;
+                buttonSelect.BackColor = Color.White;
+                ButtonRemove.BackColor = Color.White;
+            }
+        }
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            if(SelectedPicture !=null)
+            {
+                File.Delete((string)SelectedPicture.Tag);
+                buttonSelect.BackColor = Color.White;
+                ButtonRemove.BackColor = Color.White;
+                SelectedPicture = null;
+            }
+            LoadWallpapers();
+        }
+    }
+}
+
